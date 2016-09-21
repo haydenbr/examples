c568124488e9576f7f19cf947a3e910e910d59c4
haydenbr <haydenb.tester.jester@gmail.com>
2016-09-21 15:21:20 -0400

    added comments for clarification to promise chaining

--- node/promises/funkyChaining.js
+++ node/promises/funkyChaining.js
@@ -1,13 +1,23 @@
 var Q = require('q');
 
-randomAsync()
+/*
+Shows how then(), catch(), and finally() can be used together.
+
+All three functions return a promise that is resolved with the value returned from
+their callback functions. then() fires if the promise it is called on resolves,
+otherwise, then() is skipped. catch() fires if the promise it is called on rejects.
+finally() will always execute, regardless of whether or not the promise it is called
+on resolves or rejects.
+*/
+
+randomAsync()           // if any of these promises are rejected, the rest of then() blocks will be skipped
   .then(randomAsync)
   .then(randomAsync)
   .then(randomAsync)
-  .catch(restart)
+  .catch(restart)       // will only be called if one of the above promises rejects
   .then(randomAsync)
   .then(randomAsync)
-  .finally(universal)
+  .finally(universal)   // will always be called, regardles of what happens above
   .then(randomAsync)
   .catch(restart)
   .finally(universal)
@@ -19,8 +29,7 @@ randomAsync()
     console.log('done');
   });
 
-
-function randomAsync(arg) {
+function randomAsync() {
   var deferred = Q.defer();
 
   if (coinFlip()) {
@@ -34,7 +43,6 @@ function randomAsync(arg) {
   return deferred.promise;
 }
 
-
 function coinFlip() {
   return Math.floor(2*Math.random());
 }
@@ -47,4 +55,15 @@ function restart() {
 function universal() {
   console.log('I like cheetos');
   return Q();
+}
+
+function normalUseCase() { // similar to try - catch - finally
+  randomAsync()
+    .then(randomAsync)
+    .then(randomAsync)
+    .then(randomAsync)
+    .catch(function () {
+      console.log('rejected');
+    })
+    .finally(universal);
 }
